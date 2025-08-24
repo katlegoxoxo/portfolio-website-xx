b76a46da71cafb47290e37d1c3ba9f3e87ecabba
katlegoxoxo <kmakete421@gmail.com>
2025-08-24 16:14:40 +0200

    Fix GitHub Pages deployment: BrowserRouter basename, Vite base, assets

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -11,6 +11,9 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Set basename to match your GitHub Pages repo name */}
+    <BrowserRouter basename="/portfolio-website-xx">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
